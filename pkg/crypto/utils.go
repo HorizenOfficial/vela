666263444a15e71bd72a543272680e3029e3c983
paolocappelletti <paolo@horizenlabs.io>
2025-07-07 10:21:34 +0200

    more save and load meethods and unit tests

--- pkg/crypto/utils.go
+++ pkg/crypto/utils.go
@@ -145,6 +145,92 @@ func GeneratePrivateKey25519() (*common.PrivateKey25519, error) {
 	return &common.PrivateKey25519{newKey}, nil
 }
 
+// SavePrivateKey25519ToFileDER saves a 25519 private key to a file in PKCS #8, ASN.1 DER format.
+func SavePrivateKey25519ToFileDER(privKey *common.PrivateKey25519, filename string) error {
+	derBytes, err := x509.MarshalPKCS8PrivateKey(privKey.PrivateKey)
+	if err != nil {
+		return fmt.Errorf("failed to marshal private key: %w", err)
+	}
+
+	err = os.WriteFile(filename, derBytes, 0600)
+	if err != nil {
+		return fmt.Errorf("failed to write to file: %w", err)
+	}
+
+	return nil
+}
+
+// LoadPrivateKey25519FromFileDER loads a 25519 private key from a file in PKCS #8, ASN.1 DER format.
+func LoadPrivateKey25519FromFileDER(filename string) (*common.PrivateKey25519, error) {
+	derBytes, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %w", err)
+	}
+
+	key, err := x509.ParsePKCS8PrivateKey(derBytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse private key: %w", err)
+	}
+
+	ecdhKey, ok := key.(*ecdh.PrivateKey)
+	if !ok {
+		return nil, fmt.Errorf("key is not an ecdh.PrivateKey")
+	}
+
+	return &common.PrivateKey25519{ecdhKey}, nil
+}
+
+// SavePrivateKey25519ToFilePEM saves a 25519 private key to a file in PEM format.
+func SavePrivateKey25519ToFilePEM(privKey *common.PrivateKey25519, filename string) error {
+	derBytes, err := x509.MarshalPKCS8PrivateKey(privKey.PrivateKey)
+	if err != nil {
+		return fmt.Errorf("failed to marshal private key: %w", err)
+	}
+
+	pemBlock := &pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: derBytes,
+	}
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %w", err)
+	}
+	defer file.Close()
+
+	err = pem.Encode(file, pemBlock)
+	if err != nil {
+		return fmt.Errorf("failed to encode PEM: %w", err)
+	}
+
+	return nil
+}
+
+// LoadPrivateKey25519FromFilePEM loads a 25519 private key from a file in PEM format.
+func LoadPrivateKey25519FromFilePEM(filename string) (*common.PrivateKey25519, error) {
+	pemBytes, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %w", err)
+	}
+
+	pemBlock, _ := pem.Decode(pemBytes)
+	if pemBlock == nil {
+		return nil, fmt.Errorf("failed to decode PEM block")
+	}
+
+	key, err := x509.ParsePKCS8PrivateKey(pemBlock.Bytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse private key: %w", err)
+	}
+
+	ecdhKey, ok := key.(*ecdh.PrivateKey)
+	if !ok {
+		return nil, fmt.Errorf("key is not an ecdh.PrivateKey")
+	}
+
+	return &common.PrivateKey25519{ecdhKey}, nil
+}
+
 /*
 Encrypt encrypts a generic byte array.
 Sender and receiver keys must use Elliptic Curve Diffie-Hellman over NIST curve (25519 is not suitable).
--- pkg/crypto/utils_test.go
+++ pkg/crypto/utils_test.go
@@ -2,6 +2,7 @@ package crypto
 
 import (
 	"bytes"
+	"os"
 	"testing"
 )
 
@@ -15,6 +16,54 @@ func TestGeneratePrivateKeyP521(t *testing.T) {
 	}
 }
 
+func TestSaveLoadPrivateKeyP521DER(t *testing.T) {
+	key, err := GeneratePrivateKeyP521()
+	if err != nil {
+		t.Fatalf("failed to generate private key: %v", err)
+	}
+
+	filename := "test_key.der"
+	defer os.Remove(filename)
+
+	err = SavePrivateKeyP521ToFileDER(key, filename)
+	if err != nil {
+		t.Fatalf("failed to save private key: %v", err)
+	}
+
+	loadedKey, err := LoadPrivateKeyP521FromFileDER(filename)
+	if err != nil {
+		t.Fatalf("failed to load private key: %v", err)
+	}
+
+	if !bytes.Equal(key.Bytes(), loadedKey.Bytes()) {
+		t.Fatal("loaded key does not match original key")
+	}
+}
+
+func TestSaveLoadPrivateKeyP521PEM(t *testing.T) {
+	key, err := GeneratePrivateKeyP521()
+	if err != nil {
+		t.Fatalf("failed to generate private key: %v", err)
+	}
+
+	filename := "test_key.pem"
+	defer os.Remove(filename)
+
+	err = SavePrivateKeyP521ToFilePEM(key, filename)
+	if err != nil {
+		t.Fatalf("failed to save private key: %v", err)
+	}
+
+	loadedKey, err := LoadPrivateKeyP521FromFilePEM(filename)
+	if err != nil {
+		t.Fatalf("failed to load private key: %v", err)
+	}
+
+	if !bytes.Equal(key.Bytes(), loadedKey.Bytes()) {
+		t.Fatal("loaded key does not match original key")
+	}
+}
+
 func TestGeneratePrivateKey25519(t *testing.T) {
 	key, err := GeneratePrivateKey25519()
 	if err != nil {
@@ -22,6 +71,54 @@ func TestGeneratePrivateKey25519(t *testing.T) {
 	}
 	if key == nil {
 		t.Fatal("key is nil")
+	}
+}
+
+func TestSaveLoadPrivateKey25519DER(t *testing.T) {
+	key, err := GeneratePrivateKey25519()
+	if err != nil {
+		t.Fatalf("failed to generate private key: %v", err)
+	}
+
+	filename := "test_key_25519.der"
+	defer os.Remove(filename)
+
+	err = SavePrivateKey25519ToFileDER(key, filename)
+	if err != nil {
+		t.Fatalf("failed to save private key: %v", err)
+	}
+
+	loadedKey, err := LoadPrivateKey25519FromFileDER(filename)
+	if err != nil {
+		t.Fatalf("failed to load private key: %v", err)
+	}
+
+	if !bytes.Equal(key.Bytes(), loadedKey.Bytes()) {
+		t.Fatal("loaded key does not match original key")
+	}
+}
+
+func TestSaveLoadPrivateKey25519PEM(t *testing.T) {
+	key, err := GeneratePrivateKey25519()
+	if err != nil {
+		t.Fatalf("failed to generate private key: %v", err)
+	}
+
+	filename := "test_key_25519.pem"
+	defer os.Remove(filename)
+
+	err = SavePrivateKey25519ToFilePEM(key, filename)
+	if err != nil {
+		t.Fatalf("failed to save private key: %v", err)
+	}
+
+	loadedKey, err := LoadPrivateKey25519FromFilePEM(filename)
+	if err != nil {
+		t.Fatalf("failed to load private key: %v", err)
+	}
+
+	if !bytes.Equal(key.Bytes(), loadedKey.Bytes()) {
+		t.Fatal("loaded key does not match original key")
 	}
 }
 
