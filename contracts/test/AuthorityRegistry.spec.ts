995d57728d6e4aa85a897c076d085b5827c8551f
paolocappelletti <56390199+paolocappelletti@users.noreply.github.com>
2025-08-27 14:59:06 +0200

    Update AuthorityRegistry.spec.ts

--- contracts/test/AuthorityRegistry.spec.ts
+++ contracts/test/AuthorityRegistry.spec.ts
@@ -52,8 +52,10 @@ describe('AuthorityRegistry Test', function () {
     })
 
     it('cant add already added', async function () {
-        authorityRegistry.connect(signers[0]).addAllowedAuthority(APPLICATION_ID, testAddr);
-
+        await expect( 
+            authorityRegistry.connect(signers[0]).addAllowedAuthority(APPLICATION_ID, testAddr)
+        ).to.emit(authorityRegistry, "AddedAuthority").withArgs(APPLICATION_ID, testAddr);
+        
         await expect( 
             authorityRegistry.connect(signers[0]).addAllowedAuthority(APPLICATION_ID, testAddr)
         ).to.be.revertedWithCustomError(authorityRegistry, "AuthorityAlreadyPresent");
