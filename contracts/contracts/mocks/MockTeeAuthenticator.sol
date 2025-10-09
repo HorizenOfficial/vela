616d797c9ce0063b135b877edd8bf3a790c796ed
ANDREA NISTICO <nistic.andrea@gmail.com>
2025-10-09 15:23:33 +0200

    fix merge

--- contracts/contracts/mocks/MockTeeAuthenticator.sol
+++ contracts/contracts/mocks/MockTeeAuthenticator.sol
@@ -12,11 +12,11 @@ contract MockTeeAuthenticator is ITeeAuthenticator {
     }
 
     function checkSignature(
-        uint256 /*applicationId*/,
-        bytes32 /*prevStateRoot*/,
-        bytes32 /*newStateRoot*/,
-        bytes32 /*processedRequestId*/,
-        bytes[] memory /*events*/,
+        uint256 applicationId,
+        bytes32 prevStateRoot,
+        bytes32 newStateRoot,
+        bytes32 processedRequestId,
+        bytes[] memory events,
         Structs.WithdrawalRequest[] memory /*withdrawalRequests*/,
         bytes calldata /*signature*/
     ) external pure override returns (bool) {
--- tests/system/mocked_app_system_test.go
+++ tests/system/mocked_app_system_test.go
@@ -23,7 +23,7 @@ func TestDeployApp(t *testing.T) {
 	err = suite.StartManager()
 	require.NoError(t, err)
 
-	RequestID := "1233"
+	RequestID := "233"
 	ApplicationId := "1"
 
 	// 4. Submit deploy request
