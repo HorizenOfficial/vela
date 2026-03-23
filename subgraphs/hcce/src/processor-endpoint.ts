96b004f76af05d9993f97daed5e8e21809aee529
saratnt <sara@horizenlabs.io>
2026-03-23 10:51:28 +0100

    Updated subgraph for multi-app support

--- subgraphs/hcce/src/processor-endpoint.ts
+++ subgraphs/hcce/src/processor-endpoint.ts
@@ -3,6 +3,8 @@ import {
   RequestSubmitted as RequestSubmittedEvent,
   RequestCompleted as RequestCompletedEvent,
   UserEvent as UserEventEvent,
+  DeployRequestSubmitted as DeployRequestSubmittedEvent,
+  DeployRequestCompleted as DeployRequestCompletedEvent,
 } from "../generated/ProcessorEndpoint/ProcessorEndpoint";
 import { RequestSubmitted, RequestCompleted, UserEvent } from "../generated/schema";
 
@@ -12,6 +14,7 @@ export function handleRequestSubmitted(event: RequestSubmittedEvent): void {
   const id = event.transaction.hash.concatI32(event.logIndex.toI32()).toHex();
   let entity = new RequestSubmitted(id);
 
+  entity.applicationId = event.params.applicationId;
   entity.requestId = event.params.requestId;
   entity.sender = event.params.sender;
   entity.blockNumber = event.block.number;
@@ -24,6 +27,7 @@ export function handleRequestCompleted(event: RequestCompletedEvent): void {
   const id = event.transaction.hash.concatI32(event.logIndex.toI32()).toHex();
   let entity = new RequestCompleted(id);
 
+  entity.applicationId = event.params.applicationId;
   entity.requestId = event.params.requestId;
   entity.applicationFees = event.params.applicationFees;
   entity.status = event.params.status;
@@ -46,6 +50,36 @@ export function handleUserEvent(event: UserEventEvent): void {
   entity.blockNumber = event.block.number;
   entity.logIndex = event.logIndex;
   entity.sortKey = event.block.number.times(SORT_BASE).plus(event.logIndex);
+  entity.blockTimestamp = event.block.timestamp;
+
+  entity.save();
+}
+
+
+export function handleDeployRequestSubmitted(event: DeployRequestSubmittedEvent): void {
+  const id = event.transaction.hash.concatI32(event.logIndex.toI32()).toHex();
+  let entity = new RequestSubmitted(id);
+
+  entity.applicationId = event.params.applicationId;
+  entity.requestId = event.params.requestId;
+  entity.sender = event.params.sender;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+
+  entity.save();
+}
+
+export function handleDeployRequestCompleted(event: DeployRequestCompletedEvent): void {
+  const id = event.transaction.hash.concatI32(event.logIndex.toI32()).toHex();
+  let entity = new RequestCompleted(id);
+
+  entity.applicationId = event.params.applicationId;
+  entity.requestId = event.params.requestId;
+  entity.applicationFees = event.params.applicationFees;
+  entity.status = event.params.status;
+  entity.errorCode = event.params.errorCode;
+  entity.errorMessage = event.params.errorMessage;
+  entity.blockNumber = event.block.number;
   entity.blockTimestamp = event.block.timestamp;
 
   entity.save();
