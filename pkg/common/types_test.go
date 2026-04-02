e1f1e9a8b849c899ecd53aaa3e5a628e15acf561
Alberto Sala <albertosala@horizenlabs.io>
2026-04-02 15:41:43 +0200

    Added missing go file

---
+++ pkg/common/types_test.go
@@ -0,0 +1,50 @@
+package common
+
+import (
+	"testing"
+
+	ethCommon "github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRequestValidate_ZeroAssetAmountWithZeroToken(t *testing.T) {
+	req := &Request{
+		Timestamp:    NewBig(1000),
+		TokenAddress: ethCommon.Address{},
+		AssetAmount:  NewBig(0),
+		MaxFeeValue:  NewBig(100),
+	}
+	require.NoError(t, req.Validate())
+}
+
+func TestRequestValidate_NonZeroAssetAmountWithNonZeroToken(t *testing.T) {
+	req := &Request{
+		Timestamp:    NewBig(1000),
+		TokenAddress: ethCommon.HexToAddress("0xdead000000000000000000000000000000000001"),
+		AssetAmount:  NewBig(500),
+		MaxFeeValue:  NewBig(100),
+	}
+	require.NoError(t, req.Validate())
+}
+
+func TestRequestValidate_NonZeroAssetAmountWithZeroToken(t *testing.T) {
+	req := &Request{
+		Timestamp:    NewBig(1000),
+		TokenAddress: ethCommon.Address{},
+		AssetAmount:  NewBig(500),
+		MaxFeeValue:  NewBig(100),
+	}
+	require.NoError(t, req.Validate())
+}
+
+func TestRequestValidate_ZeroAssetAmountWithNonZeroToken(t *testing.T) {
+	req := &Request{
+		Timestamp:    NewBig(1000),
+		TokenAddress: ethCommon.HexToAddress("0xdead000000000000000000000000000000000001"),
+		AssetAmount:  NewBig(0),
+		MaxFeeValue:  NewBig(100),
+	}
+	err := req.Validate()
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "tokenAddress must be zero address when assetAmount is zero")
+}
